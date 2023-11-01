f0c434918cc020e790447ce6200a9100827748d3
himanshu1998-ai <himanshuchauhan6@gmail.com>
2023-11-01 12:56:37 +0530

    using env file and deleetd config file in db

--- BLOG/src/blog/entrypoint/postgresql/config.py
+++
@@ -1,7 +0,0 @@
-host = "172.17.0.2"
-port = 5432
-user = "postgres"
-password = "mypasssecret"
-dbname = "postgres"
-dbtype = "postgresql"
-
--- BLOG/src/blog/entrypoint/postgresql/database.py
+++ BLOG/src/blog/entrypoint/postgresql/database.py
@@ -1,10 +1,11 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from typing import Generator
-from .config import dbtype, user, password, host, port, dbname
 SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
-# SQLALCHEMY_DATABASE_URL = f"{dbtype}://{user}:{password}@{host}:{port}/{dbname}"
+# SQLALCHEMY_DATABASE_URL = f"{os.getenv('DBTYPE')}://{os.getenv('USER')}:{os.getenv('PASSWORD')}" \
+#                           f"@{os.getenv('HOST')}:{os.getenv('PORT')}/{os.getenv('DBNAME')}"
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
